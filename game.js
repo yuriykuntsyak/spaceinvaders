1a67da88a0dc73462fea48b567347218d43ff85d
Yuriy <yuriy-kuntsyak@mail.ru>
2016-07-20 20:13:19 +0200

    L2: game loop

--- game.js
+++ game.js
@@ -2,13 +2,36 @@
 {
 	var Game = function(canvasId)
 	{
-		console.log("Hello");
+		var canvas = document.getElementById(canvasId);
+		var screen = canvas.getContext('2d');
+		var gameSize = {x: canvas.width, y: canvas.height};
+		var self = this;
+
+		var tick = function()
+		{
+			self.update();
+			self.draw(screen, gameSize);
+			requestAnimationFrame(tick);
+		}
+
+		tick();
 	}
 
-	Game.prototype = {	}
+	Game.prototype = 
+	{
+		update: function()
+		{
+			console.log("update");
+		},
+
+		draw: function(screen, gameSize)
+		{
+			screen.fillRect(150,150,32,32);
+		}
+	}
 
 	window.onload = function()
 	{
-		new Game("");
+		new Game("screen");
 	}
 })();
